9711efae7a8e9fd41eca0f0b908108a20ed31e08
Karinda <karinasirokova5442@gmail.com>
2023-03-24 22:21:11 +0300

    third commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int x = 100;
+        int y = 500;
+        int bonus = y/100;
+        int total = x+y+bonus;
+
+        if (y>=500) {
+            System.out.println("Колличество бонусных рублей:" +bonus);
+        } else {
+            System.out.println("Колличество бонусных рублей: 0 ");
+        }
+
+        System.out.println("Итоговый счет:" +total);
     }
 }
